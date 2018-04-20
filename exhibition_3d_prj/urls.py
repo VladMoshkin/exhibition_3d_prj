847dd7509ab9f9e0abeca3e0407077440ce3b23c
VladMoshkin <vladm08hkin@gmail.com>
2018-04-20 12:46:55 +0300

    Initial commit

--- exhibition_3d_prj/urls.py
+++ exhibition_3d_prj/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('exhibition.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
