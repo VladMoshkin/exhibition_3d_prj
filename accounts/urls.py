51f3a730765a2589cfde2755612a027c6d70ad3d
VladMoshkin <vladm08hkin@gmail.com>
2018-06-06 14:46:07 +0300

    Регистрация (register)

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.contrib.auth.views import login, logout
 
 urlpatterns = [
+    url(r'^$', views.profile, name='profile'),
     url(r'^login/$', login, {'template_name': 'accounts/login.html'}),
-    url(r'^logout/$', views.logout_user, name="logout")
+    url(r'^logout/$', views.logout_user, name="logout"),
+    url(r'^register/$', views.register, name="register"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
+from accounts.forms import RegistrationForm
+from django.contrib.auth.models import User
 
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect('/')
+
+def register(request):
+    if request.method =='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    else:
+        form = RegistrationForm()
+
+        args = {'form': form}
+        return render(request, 'accounts/register.html', args)
+
+def profile(request):
+    args = {'user': request.user}
+    return render(request, 'accounts/profile.html', args)
